f026f3cdb456a8c2c937c2f791f2bb478070ac28
s4sastue <s4sastue@uni-trier.de>
2026-05-20 14:35:59 +0200

    Integration of phorecast_ml

--- src/configurable_components/models/mosmix_model.py
+++ src/configurable_components/models/mosmix_model.py
@@ -5,13 +5,11 @@ predict the output utilizing a LSTM neural network. It also contains the form fo
 
 import os
 import random
-import tempfile
 
 import keras
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from sklearn.linear_model import LinearRegression
+import phorecast_ml
 from sqlalchemy import ForeignKey, Float, Integer
 from sqlalchemy.orm import mapped_column, Mapped
 from wtforms import IntegerField, validators
@@ -19,10 +17,7 @@ from wtforms import IntegerField, validators
 from src.configurable_components.exceptions import ComponentError
 from src.configurable_components.models.base_model import ModelForm, BaseModel, ModelRun
 from src.database.influx_interface import influx_interface
-from src.utils.dataset import attach_solar_positions, windowing, get_dataset_from_windows, \
-    split_windows, create_tf_dataset
 from src.utils.general import plot_history, plot_windows, plot_predictions
-from src.utils.keras import sum_difference_metric
 from src.utils.logging import get_default_logger
 
 logger = get_default_logger(__name__)
@@ -41,7 +36,8 @@ class DWDMosmixModelForm(ModelForm):
                                 validators=[validators.NumberRange(min=1)])
     batch_size = IntegerField('Training batch size', default=32,
                               validators=[validators.NumberRange(min=1)])
-
+    epochs = IntegerField('duration of training (epochs)', default=300,
+                              validators=[validators.NumberRange(min=1)])
 
 class DWDMosmixModelLSTM(BaseModel):
     """
@@ -68,6 +64,7 @@ class DWDMosmixModelLSTM(BaseModel):
     factor_width: Mapped[float] = mapped_column(Float)
     factor_depth: Mapped[int] = mapped_column(Integer)
     batch_size: Mapped[int] = mapped_column(Integer)
+    epochs: Mapped[int] = mapped_column(Integer)
 
     def train(self):
         """
@@ -95,16 +92,18 @@ class DWDMosmixModelLSTM(BaseModel):
             f"Retrieved Training data with {days_in_training_data:.0f} days worth of samples")
 
         # Preprocess the data
-        data = self._preprocessing(data)
+        data = phorecast_ml.preprocessing.solar.attach_solar_positions(data, self.source_loader.lat, self.source_loader.lon, self.source_loader.height)
+        data = phorecast_ml.preprocessing.filtering.solar_position_filter(data,["elevation", "azimuth", "Rad1h"], self.target_field.influx_field)
 
         # Create windows from the dataset and split them into test and train
         try:
-            windows = windowing(data, self.window_size, self.window_size // 4, 2)
+            windows = phorecast_ml.preprocessing.windowing(data, 24, 24 // 4, 2)
         except ValueError as e:
             raise ComponentError("Could not create windows from dataset, "
                                  "maybe the data is to cut up or noisy", self) from e
+
         train_test_split = DWDMosmixModelLSTM.TRAIN_TEST_SPLIT
-        train_windows, test_windows = split_windows(windows, train_test_split, factor=14)
+        train_windows, test_windows = phorecast_ml.preprocessing.dataset_splitting.split_windows(windows, test_ratio=0.24, factor=14)
 
         # check the test ratio
         actual_test_ratio = len(test_windows) / (len(test_windows) + len(train_windows))
@@ -116,30 +115,20 @@ class DWDMosmixModelLSTM(BaseModel):
                         f"{len(test_windows)} windows (window_size: {self.window_size})")
 
         # for performance reasons, the data is converted into tf datasets
-        train_ds_np, _ = get_dataset_from_windows(train_windows,
-                                                  target=self.target_field.influx_field)
-        test_ds_np, test_index = get_dataset_from_windows(test_windows,
-                                                          target=self.target_field.influx_field)
+        (train_X, train_y), _ = phorecast_ml.preprocessing.get_dataset_from_windows(
+            train_windows, target=self.target_field.influx_field)
+        (test_X, test_y), test_index = phorecast_ml.preprocessing.get_dataset_from_windows(
+            test_windows, target=self.target_field.influx_field)
 
-        train_ds = create_tf_dataset(train_ds_np, self.batch_size, shuffle=True)
-        test_ds = create_tf_dataset(test_ds_np, self.batch_size)
+        units = int(self.factor_width * len(self.PARAMETERS))
+        depth = int(self.factor_depth * len(self.PARAMETERS))
 
-        # Train the model with callbacks
-        with tempfile.TemporaryDirectory() as temp_dir:
-            keras.config.disable_traceback_filtering()
-            model = self._build_lstm(train_ds)
-
-            temp_file = os.path.join(temp_dir, "model_temp.keras")
-            checkpoint_callback = keras.callbacks.ModelCheckpoint(temp_file,
-                                                                  monitor="val_loss", mode="min",
-                                                                  save_best_only=True,
-                                                                  verbose=0)
-            early_stopping_callback = keras.callbacks.EarlyStopping(patience=30)
-
-            history = model.fit(train_ds, epochs=300, validation_data=test_ds,
-                                callbacks=[checkpoint_callback, early_stopping_callback], verbose=0)
-
-            model = keras.models.load_model(temp_file)
+        model = phorecast_ml.model.LSTM(epochs=self.epochs,
+                                        units=units,
+                                        depth=depth,
+                                        metrics=["mean_absolute_error", ],
+                                        batch_size=self.batch_size)
+        history = model.train(train_X, train_y, test_X, test_y)
 
         # Save the model and complete the run
         best_loss = min(history.history['val_loss'])
@@ -156,12 +145,12 @@ class DWDMosmixModelLSTM(BaseModel):
         plot_windows(train_windows, test_windows, os.path.join(run.path, "train_windows.jpeg"))
 
         # plot 3 test predictions
-        test_predictions = model.predict(test_ds, verbose=0)
+        test_predictions = model.predict(test_X)
         for _ in range(3):
-            index = random.randint(0, len(test_ds_np[0])-1)
-            df = pd.DataFrame({'label': test_ds_np[1][index].reshape(-1),
+            index = random.randint(0, len((test_X, test_y)[0])-1)
+            df = pd.DataFrame({'label': (test_X, test_y)[1][index].reshape(-1),
                                'prediction': test_predictions[index].reshape(-1),
-                               'reference': [x[15] for x in test_ds_np[0][index]]},
+                               'reference': [x[15] for x in (test_X, test_y)[0][index]]},
                               index=test_index[index])
             plot_predictions(df, os.path.join(run.path, f"example_{index}.jpeg"), "GLI")
 
@@ -190,9 +179,15 @@ class DWDMosmixModelLSTM(BaseModel):
             # prepare the data
             input_df = influx_interface.get_weather_forecasts(loader_id=self.source_loader.id,
                                                               run=run)
-            input_df = self._preprocessing(input_df)
-            windows = windowing(input_df, self.window_size, self.window_size, self.window_size)
-            (data, _), _ = get_dataset_from_windows(windows)
+            input_df = input_df.drop("model", axis=1)
+            input_df = phorecast_ml.preprocessing.solar.attach_solar_positions(input_df, self.source_loader.lat, self.source_loader.lon, self.source_loader.height)
+            input_df = phorecast_ml.preprocessing.filtering.solar_position_filter(input_df,
+                                                                       ["elevation", "azimuth", "Rad1h"],
+                                                                       self.target_field.influx_field)
+
+            windows = phorecast_ml.preprocessing.windowing(input_df, self.window_size, self.window_size, self.window_size)
+            (data, _), _ = phorecast_ml.preprocessing.get_dataset_from_windows(
+                windows, target=self.target_field.influx_field)
 
             # predict the data
             predictions = model.predict(data, batch_size=1, verbose=0)
@@ -206,116 +201,3 @@ class DWDMosmixModelLSTM(BaseModel):
             # write the predictions to the database
             influx_interface.write_pv_forecast(prediction_df, self.id, run)
             logger.info(f"Model {self.name} created predictions for run {run}")
-
-    def _preprocessing(self, data):
-        """
-        THis function filters the data in 3 steps and adds solar positions to it
-        :param data: dataframe with the data
-        :return: dataframe with the filtered data
-        """
-
-        lat = self.source_loader.lat
-        lon = self.source_loader.lon
-        height = self.source_loader.height
-        target = self.target_field.influx_field
-
-        data = attach_solar_positions(data, lat, lon, height)
-        original_length = len(data)
-
-        missing_cols = [x for x in self.PARAMETERS if x not in
-                        data.columns]
-
-        if len(missing_cols) > 0:
-            logger.warning(f"Training data preprocessing: Missing columns in data: {missing_cols}")
-
-        for m in missing_cols:
-            data[m] = 0
-
-        if target not in data.columns:
-            return data[self.PARAMETERS]
-
-        # Rule 1: If elevation < -10, target has to be 0
-        data = data[~((data['elevation'] < -10) & (data[target] > 0))].copy()
-        rule_1_count = original_length - len(data)
-
-        # Rule 2: Remove rows where elevation > 10 and target is 0
-        data = data[~((data['elevation'] > 10) & (data[target] == 0))].copy()
-        rule_2_count = (original_length - len(data)) - rule_1_count
-        # Rule 3: Remove outliers with Linear Regression
-        x = data[['elevation', 'azimuth', 'Rad1h']]  # Independent variable
-        y = data[self.target_field.influx_field]  # Dependent variable
-
-        # Fit the model and predict
-        model = LinearRegression()
-        model.fit(x, y)
-        y_pred = model.predict(x)
-
-        # calculate residuals
-        data['__residuals'] = y - y_pred
-        residual_std = np.std(data['__residuals'])
-        residual_mean = np.mean(data['__residuals'])
-        threshold = 5 * residual_std
-
-        condition = (data['__residuals'] <= (residual_mean + threshold)) & (
-                data['__residuals'] >= (residual_mean - threshold))
-
-        data_filtered = data.loc[condition].copy()
-        rule_3_count = original_length - len(data_filtered) - rule_1_count - rule_2_count
-
-        logger.info(
-            f"Removed {(rule_1_count / original_length) * 100:.2f}% "
-            f"of rows with elevation < -10 and target > 0")
-        logger.info(
-            f"Removed {(rule_2_count / original_length) * 100:.2f}% "
-            f"of rows with elevation > 10 and target = 0")
-        logger.info(
-            f"Removed {(rule_3_count / original_length) * 100:.2f}% "
-            f"of rows with Linear Regression")
-
-        data_filtered.drop(columns=['__residuals'], inplace=True)
-
-        return data_filtered[[*self.PARAMETERS, target]]
-
-    def _build_lstm(self, data: tf.data.Dataset) -> keras.models.Sequential:
-        """
-        Builds a LSTM model with the given hyperparameters
-        :param data: give data to adapt the normalization layer
-        :return: build and fitted keras model
-        """
-        # define model hyperparameters
-        learning_rate = 0.001
-        loss = keras.losses.MeanAbsoluteError()
-        features = len(self.PARAMETERS)
-        width = int(features * self.factor_width)
-        model = keras.models.Sequential()
-        batch_size = self.batch_size if data is not None else 1
-
-        # create input (InputLayer, MaskingLayer, NormalizationLayer)
-        model.add(keras.layers.Input((100, features), batch_size=batch_size))
-        model.add(keras.layers.Masking(mask_value=0))
-        # create and adapt normalization layer
-        normalization_layer = keras.layers.Normalization(axis=-1)
-        model.add(normalization_layer)
-        normalization_layer.adapt(data=data.map(lambda x, y: x))
-
-        # create body of model (LSTM layers)
-        for _ in range(self.factor_depth):
-            model.add(keras.layers.LSTM(units=width,
-                                        return_sequences=True,
-                                        dropout=0.2))
-
-        # create output layer (DenseLayer, inverted NormalizationLayer)
-        model.add(keras.layers.Dense(units=1, activation='linear'))
-        # create and adapt normalization layer
-        normalization_layer_output = keras.layers.Normalization(axis=-1, invert=True)
-        model.add(normalization_layer_output)
-        normalization_layer_output.adapt(data=data.map(lambda x, y: y))
-
-        # compile model
-        model.compile(optimizer=keras.optimizers.Adam(learning_rate), loss=loss,
-                      metrics=['mean_absolute_error', sum_difference_metric])
-
-        logger.debug(f"Built LSTM model with width: {width}, depth: {self.factor_depth}, "
-                     f"batch_size: {batch_size}")
-
-        return model
